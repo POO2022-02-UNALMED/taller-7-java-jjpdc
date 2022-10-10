3e6e1068c69228100919ed59a098cb833f4f7ef4
Juan Pablo Diaz Cervantes <53982528+jjpdc@users.noreply.github.com>
2022-10-10 15:51:05 -0500

    tesis

--- src/main/java/comunicacion/Tesis.java
+++ src/main/java/comunicacion/Tesis.java
@@ -1,44 +1,58 @@
 package comunicacion;
 
-public abstract class Escrito extends Pictograma{
-	
-	private String titulo;
-	private String autor;
-	private int paginas;
-	public Escrito(String origen, String titulo, String autor, int paginas) {
-		super(origen);
-		this.titulo = titulo;
-		this.autor = autor;
-		this.paginas = paginas;
-	}
-	
-	public String getTitulo() {
-		return titulo;
-	}
-	public void setTitulo(String titulo) {
-		this.titulo = titulo;
-	}
-	public String getAutor() {
-		return autor;
-	}
-	public void setAutor(String autor) {
-		this.autor = autor;
-	}
-	public int getPaginas() {
-		return paginas;
-	}
-	public void setPaginas(int paginas) {
-		this.paginas = paginas;
-	}
-	
-	public String resumen() {
-		String r = this.getOrigen() + "\n";
-		r += this.titulo + "\n";
-		r += this.autor + "\n";
-		r += this.paginas;
-		return  r;
-	}
-	
-	abstract int palabrasTotales(int palabrasPagina);
-	
+public class Tesis extends Escrito {
+    private String idea;
+    private String[] argumentos;
+    private String conclusion;
+    private String referencias;
+    private String interpretacion;
+    public Tesis(String origen, String titulo, String autor, int paginas, String idea, String[] argumentos,
+            String conclusion, String referencias, String interpretacion) {
+        super(origen, titulo, autor, paginas);
+        this.idea = idea;
+        this.argumentos = argumentos;
+        this.conclusion = conclusion;
+        this.referencias = referencias;
+        this.interpretacion = interpretacion;
+    }
+    public int palabrasTotales(int palabrasPagina){
+        return this.getPaginas()*palabrasPagina*5;
+    }
+    public String interpretacion(){
+        return this.interpretacion;
+    }
+    public String toString(){
+        return (this.getOrigen()+"\n"+this.getTitulo()+"\n"+this.getAutor()+"\n"+this.getPaginas()+"\n"+this.getIdea()+"\n"+this.getArgumentos().length+
+        "\n"+this.getConclusion()+"\n"+this.getReferencias());
+    }
+    public String getIdea() {
+        return idea;
+    }
+    public void setIdea(String idea) {
+        this.idea = idea;
+    }
+    public String[] getArgumentos() {
+        return argumentos;
+    }
+    public void setArgumentos(String[] argumentos) {
+        this.argumentos = argumentos;
+    }
+    public String getConclusion() {
+        return conclusion;
+    }
+    public void setConclusion(String conclusion) {
+        this.conclusion = conclusion;
+    }
+    public String getReferencias() {
+        return referencias;
+    }
+    public void setReferencias(String referencias) {
+        this.referencias = referencias;
+    }
+    public String getInterpretacion() {
+        return interpretacion;
+    }
+    public void setInterpretacion(String interpretacion) {
+        this.interpretacion = interpretacion;
+    }
 }
